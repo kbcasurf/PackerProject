43702e70af51235232dbabd8d41467d6d50d5e34
kbcasurf <107873401+kbcasurf@users.noreply.github.com>
2022-08-27 12:52:31 -0300

    Delete files

--- golden/run-promtail.sh
+++
@@ -1,3 +0,0 @@
-# Run promtail in background
-cd /opt/promtail
-nohup ./promtail-linux-amd64 -config.file=./promtail.yaml &
--- hashicups/start-hashicups.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Start HashiCups in background
-cd /home/ubuntu
-sudo docker-compose up -d
